dd2a2ee0b5fd55bb255f574d09843a8da0782729
licheng.hata <licheng.hata@bytedance.com>
2022-06-20 20:29:23 +0800

    feat: guess num

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+// use ferris_says::say;
+// use std::io::{stdout, BufWriter};
+
+fn guess_num() {
+    println!("guess num!"); // ! 宏
+    let mut guess = String::new(); //  mut 可变类型  :: 关联方法
+
+    std::io::stdin().read_line(&mut guess).expect("read error");  // & 引用
+
+    println!("you guess num is: {}", guess)  
+}
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    guess_num();
+    // let stdout = stdout();
+    // let message = String::from("Hello fellow Rustaceans!");
+    // let width = message.chars().count();
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    // let mut writer = BufWriter::new(stdout.lock());
+    // say(message.as_bytes(), width, &mut writer).unwrap();
 }
