a0b32e46fc1526726d4576fea35f70a0390021d7
licheng.hata <licheng.hata@bytedance.com>
2022-06-20 21:13:50 +0800

    feat: guess num

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,47 @@
 // use ferris_says::say;
 // use std::io::{stdout, BufWriter};
+use rand::Rng;
+use std::cmp::Ordering;
 
-fn guess_num() {
+fn guess_num() -> String {
     println!("guess num!"); // ! 宏
     let mut guess = String::new(); //  mut 可变类型  :: 关联方法
 
-    std::io::stdin().read_line(&mut guess).expect("read error");  // & 引用
+    std::io::stdin().read_line(&mut guess).expect("read error"); // & 引用
 
-    println!("you guess num is: {}", guess)  
+    println!("you guess num is: {}", guess);
+    return guess;
+}
+
+fn gen_secret_num() -> u32 {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("secret num is: {}", secret_number);
+    return secret_number;
+}
+
+fn cmp_num(secret_num: u32) {
+    loop {
+        let guess_num_string: String = guess_num();
+        let guess_num: u32 = match guess_num_string.trim().parse() {
+            Ok(num) => num,
+            Err(err) => {
+                println!("Please type a number! Err:{}", err);
+                continue;
+            }
+        };
+        match guess_num.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"), // match's arm
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
 
 fn main() {
-    guess_num();
+    cmp_num(gen_secret_num());
     // let stdout = stdout();
     // let message = String::from("Hello fellow Rustaceans!");
     // let width = message.chars().count();
